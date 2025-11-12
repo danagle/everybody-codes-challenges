d06a2f79870e7c440e9912a8282079642c587edf
Dan Nagle <752694+danagle@users.noreply.github.com>
2025-11-12 12:57:55 +0000

    Count with recursion

--- 2025-the-songs-of-ducks-and-dragons/quest07.py
+++ 2025-the-songs-of-ducks-and-dragons/quest07.py
@@ -4,6 +4,7 @@ Quest 7: Namegraph
 https://everybody.codes/event/2025/quests/7
 """
 from collections import Counter
+from functools import cache
 from itertools import pairwise, product
 from pathlib import Path
 
@@ -40,6 +41,36 @@ def part2(filepath: str = "../input/everybody_codes_e2025_q07_p2.txt") -> None:
     print("Part 2:", total)
 
 
+def count_iterative(prefixes, mapping):
+    shortest, longest, total = 7, 11, 0
+    for prefix in prefixes:
+        last_chars = Counter(prefix[-1])
+        length = len(prefix)
+        while length < longest:
+            length += 1
+            new_last_chars = Counter()
+            for char, count in last_chars.items():
+                for next_char in mapping.get(char, ()):
+                    new_last_chars[next_char] += count
+            last_chars = new_last_chars
+            if length >= shortest:
+               total += sum(last_chars.values())
+    return total
+
+
+def count_recursive(prefixes, mapping):
+    @cache
+    def count(last_char, length):
+        if length > 11:
+            return 0
+        total = int(length >= 7)
+        for next_char in mapping.get(last_char, ()):
+            total += count(next_char, length + 1)
+        return total
+
+    return sum(count(prefix[-1], len(prefix)) for prefix in prefixes)
+
+
 def part3(filepath: str = "../input/everybody_codes_e2025_q07_p3.txt") -> None:
     prefixes, rules = load_file(filepath)
 
@@ -55,22 +86,13 @@ def part3(filepath: str = "../input/everybody_codes_e2025_q07_p3.txt") -> None:
             if all(pair in pairs for pair in pairwise(prefix)):
                 filtered_prefixes.append(prefix)
 
-    shortest, longest, num_unique = 7, 11, 0
-
-    for prefix in filtered_prefixes:
-        last_chars = Counter(prefix[-1])
-        length = len(prefix)
-        while length <= longest - 1:
-            length += 1
-            new_last_chars = Counter()
-            for char, count in last_chars.items():
-                for next_char in mapping.get(char, ()):
-                    new_last_chars[next_char] += count
-            last_chars = new_last_chars
-            if length >= shortest:
-                num_unique += sum(last_chars.values())
-
-    print("Part 3:", num_unique)
+    # python -m cProfile -s time quest07.py
+    # 8874 function calls (8790 primitive calls) in 0.011 seconds
+    # total = count_iterative(filtered_prefixes, mapping)
+    # 6324 function calls (6093 primitive calls) in 0.008 seconds
+    total = count_recursive(filtered_prefixes, mapping)
+    
+    print("Part 3:", total)
 
 
 if __name__ == "__main__":
